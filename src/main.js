9d68fd7f0aedef474e681709d8207b98508d7e0c
jatsala <jatsalaberria@gmail.com>
2025-10-10 19:04:28 -0400

    agregate authMidleware

--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
-// import AppExpress from "@itznotabug/appexpress";
-// const app = new AppExpress();
-// import userRoutes from "./routes/user.js";
-// import authMiddleware from "./middleware/auth.js";
-
-// app.middleware(authMiddleware);
-// app.use("/user", userRoutes);
-
-// const getRoutes = (request, response) => {
-//     response.json({ routes: ["/", "/user/:username", "/user"] });
-// };
-
-// app.get("/", getRoutes);
-
-// export default async (context) => await app.attach(context);
-
 import AppExpress from "@itznotabug/appexpress"
 const app = new AppExpress()
 import userRoutes from './routes/user.js'
+import authMiddleware from './middleware/auth.js'
 
+app.middleware(authMiddleware)
 app.use("/user", userRoutes)
 
 const getRoutes = (request, response) => {
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,21 +1,3 @@
-// import AppExpress from "@itznotabug/appexpress";
-// const router = new AppExpress.Router();
-
-// const getUser = (request, response) => {
-//     const { username } = request.params;
-//     response.json({ username: username });
-// };
-
-// const createUser = (request, response) => {
-//     response.json({ postData: request.body });
-// };
-
-// router.get("/:username", getUser);
-// router.post("/", createUser);
-
-// export default router;
-
-/****Desde aca nuevo codigo */
 import AppExpress from "@itznotabug/appexpress"
 const router = new AppExpress.Router()
 
