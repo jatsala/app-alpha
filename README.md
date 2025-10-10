7fc6c1ef23cb9b8ef64d94cc17e24dcc7d27453c
jatsala <jatsalaberria@gmail.com>
2025-10-10 11:13:32 -0400

    -

---
+++ README.md
@@ -0,0 +1,12 @@
+# AppExpress demo code
+
+```js
+router.get("/users", getUsers);
+
+router.post("/users", addUser);
+
+router.get("/users/:id", getUser);
+
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
+```
---
+++ src/main.js
@@ -0,0 +1,37 @@
+// import AppExpress from "@itznotabug/appexpress";
+// const app = new AppExpress();
+// import userRoutes from "./routes/user.js";
+// import authMiddleware from "./middleware/auth.js";
+
+// app.middleware(authMiddleware);
+// app.use("/user", userRoutes);
+
+// const getRoutes = (request, response) => {
+//     response.json({ routes: ["/", "/user/:username", "/user"] });
+// };
+
+// app.get("/", getRoutes);
+
+// export default async (context) => await app.attach(context);
+
+import AppExpress from "@itznotabug/appexpress"
+const app = new AppExpress()
+
+const getRoutes = (request, response) => {
+    response.json({ routes: ["/", "/user/:username", "/user"] })
+}
+
+const getUser = (request, response) => {
+    const { username } = request.params
+    response.json({ 'username': username })
+}
+
+const createUser = (request, response) => {
+    response.json({ 'postData': request.body })
+}
+
+app.get("/", getRoutes)
+app.get("/user/:username", getUser)
+app.post("/user", createUser)
+
+export default async (context) => await app.attach(context)
