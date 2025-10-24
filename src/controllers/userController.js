540598a3c8f0808a7ce961b2b614f8685ee7f895
jatsala <jatsalaberria@gmail.com>
2025-10-24 11:44:08 -0400

    fix  mvc structure v1.13 fix express-router.route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 //import { } from '../utils/appError.js'
 
 /**Test EndPoint */
-export function getUser(req, res, log, error) {
+export function getUser(req, res) {
     const { username } = req.params
     res.json({
         'status': 200,
@@ -9,7 +9,7 @@ export function getUser(req, res, log, error) {
     })
 }
 
-export function createUser(req, res, log, error) {
+export function createUser(req, res) {
     const { data } = req.body
     res.json({
         'status': 201,
