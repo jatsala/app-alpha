39b641e4425167eeac51c4fd71822a21dc94184b
jatsala <jatsalaberria@gmail.com>
2025-10-23 20:49:04 -0400

    --v1.2

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,12 @@
 import AppExpress from "@itznotabug/appexpress"
 const router = new AppExpress.Router()
 
-const getUser = (req, res) => {
+const getUser = (req, res, log, error) => {
     const { username } = req.params
     res.json({ 'username': username })
 }
 
-const createUser = (req, res) => {
+const createUser = (req, res, log, error) => {
     res.json({ 'postData': req.body })
 }
 
