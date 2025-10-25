730174d455f1d5f732d761b18797c9ecbfba3708
jatsala <jatsalaberria@gmail.com>
2025-10-25 18:48:43 -0400

    ---**

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -10,4 +10,4 @@ export const PROJECT_ID = process.env.PROJECT_ID
 // const DATABASE_ID = process.env.DATABASE_ID
 export const COLLECTION_ID = process.env.COLLECTION_ID
 
-console.log(appwriteConfig.PROJECT_ID)
+console.log(PROJECT_ID)
