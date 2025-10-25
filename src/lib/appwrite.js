c736bdf3f17e57dc71f842af36eb9653a45f077b
jatsala <jatsalaberria@gmail.com>
2025-10-25 18:27:50 -0400

    --

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,7 +1,9 @@
-import { Client, Databases } from 'node-appwrite'
+// import { Client, Databases } from 'node-appwrite'
 
 export const appwriteConfig = {
     PROJECT_ID: process.env.PROJECT_ID
     DATABASE_ID: process.env.DATABASE_ID
     COLLECTION_ID: process.env.COLLECTION_ID
 }
+
+console.log(appwriteConfig.PROJECT_ID)
