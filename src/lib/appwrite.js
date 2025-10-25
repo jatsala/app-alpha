dc01076f90849234f5a883cd0ca5f3356a6cf53e
jatsala <jatsalaberria@gmail.com>
2025-10-25 19:13:36 -0400

    ***--

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -3,7 +3,7 @@
 export const appwriteConfig = {
     PROJECT_ID: process.env.PROJECT_ID,
     COLLECTION_ID: process.env.COLLECTION_ID,
-    DATABASE_ID = process.env.DATABASE_ID,
+    DATABASE_ID: process.env.DATABASE_ID,
 }
 
 // export const PROJECT_ID = process.env.PROJECT_ID
