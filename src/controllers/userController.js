00d6db4c383962e1d3831e4cc35f1d30152bf177
jatsala <jatsalaberria@gmail.com>
2025-10-26 09:15:13 -0400

    ****--

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,12 @@
-import { PROJECT_ID, DATABASE_ID } from '../lib/appwrite.js'
+import { appwriteConfig } from '../lib/appwrite.js'
 /* First EndPoin */
 export function getUser(req, res, log, error) {
     const { username } = req.params
     res.json({
         'status': 200,
         'username': username,
-        'projectId': PROJECT_ID,
-        'databaseId': DATABASE_ID,
+        'projectId': appwriteConfig.PROJECT_ID,
+        'databaseId': appwriteConfig.DATABASE_ID,
     })
 }
 
