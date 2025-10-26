5e6867f4959da4cd5adf36e3fceaa4547f87c8d8
jatsala <jatsalaberria@gmail.com>
2025-10-26 10:32:29 -0400

    fix db conextion v1.4.4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,21 +1,8 @@
 import { appwriteConfig, client, db, } from '../lib/appwrite.js'
-// import { Databases } from 'node-appwrite'
-// import { Databases, } from 'appwrite'
-
 
 /* First EndPoin */
 export async function getUser(req, res, log, error) {
-    // const { username } = req.params
-    // res.json({
-    //     'status': 200,
-    //     'username': username,
-    //     'projectId': appwriteConfig.PROJECT_ID,
-    //     'databaseId': appwriteConfig.DATABASE_ID,
-    // })
-
-
     const { username } = req.params
-    // const db = new Databases(client);
     const response = await db.listDocuments({
         databaseId: appwriteConfig.DATABASE_ID,
         collectionId: appwriteConfig.COLLECTION_ID,
