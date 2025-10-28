ce563d3630e619917d8b607cf1362724de7fe0f8
jatsala <jatsalaberria@gmail.com>
2025-10-28 19:53:50 -0400

    fix file appwrite.js

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -2,19 +2,21 @@
 import { Client, Query, TablesDB, } from 'appwrite'
 
 
-export const appwriteConfig = {
+const appwriteConfig = {
     PROJECT_ID: process.env.PROJECT_ID,
     COLLECTION_ID: process.env.COLLECTION_ID,
     DATABASE_ID: process.env.DATABASE_ID,
 }
 
-export const client = new Client()
+const client = new Client()
     .setEndpoint('https://nyc.cloud.appwrite.io/v1')
     .setProject(appwriteConfig.PROJECT_ID);
 
 // export const db = new Databases(client);
 
-export const tablesDB = new TablesDB(client);
+const tablesDB = new TablesDB(client);
+
+export { appwriteConfig, client, tablesDB }
 
 
 
